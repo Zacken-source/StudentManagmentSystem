5e563023d4de0ef30462642265c34d5c57e23ca3
zacken-source <marino.chereaudemedina@ynov.com>
2025-02-20 11:05:21 +0100

    dfsd

---
+++ src/main/java/org/example/Class.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Class {
+    private String courseName;
+    private int courseCode;
+    private int creditHours;
+    private ArrayList<Object> students;
+
+    public void enrollStudent(Object student){
+        this.students.add(student);
+    }
+
+
+}
